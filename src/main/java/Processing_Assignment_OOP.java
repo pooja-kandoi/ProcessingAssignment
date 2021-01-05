0f124c3a9f75f7d83405ff78a6aa1f8b17a0d7e3
pooja-kandoi <pooja.kandoi@thoughtworks.com>
2021-01-05 11:50:28 +0530

    formatted the code

--- src/main/java/Processing_Assignment_OOP.java
+++ src/main/java/Processing_Assignment_OOP.java
@@ -6,33 +6,35 @@ public class Processing_Assignment_OOP extends PApplet {
     public static final int HEIGHT = 480;
     public static final int DIAMETER = 10;
 
-    public static class MovingBall{
+    public static class MovingBall {
         int xAxis;
         int yAxis;
         int speed;
 
-        public MovingBall(int x, int y, int speed){
-            this.xAxis=x;
-            this.yAxis=y;
-            this.speed=speed;
+        public MovingBall(int x, int y, int speed) {
+            this.xAxis = x;
+            this.yAxis = y;
+            this.speed = speed;
         }
     }
+
     MovingBall[] Balls;
-    public static void main(String[] args){
-        PApplet.main("Processing_Assignment_OOP",args);
+
+    public static void main(String[] args) {
+        PApplet.main("Processing_Assignment_OOP", args);
     }
 
     @Override
     public void settings() {
         super.settings();
-        size(WIDTH,HEIGHT);
+        size(WIDTH, HEIGHT);
     }
 
     @Override
     public void setup() {
-        Balls=new MovingBall[4];
-        for(int i=0;i<4;i++){
-            Balls[i]=new MovingBall(0,(HEIGHT*(i+1))/5,i+1);
+        Balls = new MovingBall[4];
+        for (int i = 0; i < 4; i++) {
+            Balls[i] = new MovingBall(0, (HEIGHT * (i + 1)) / 5, i + 1);
         }
     }
 
@@ -45,6 +47,6 @@ public class Processing_Assignment_OOP extends PApplet {
     }
 
     private void drawCircle(MovingBall ball) {
-        ellipse(ball.xAxis,ball.yAxis,DIAMETER,DIAMETER);
+        ellipse(ball.xAxis, ball.yAxis, DIAMETER, DIAMETER);
     }
 }
